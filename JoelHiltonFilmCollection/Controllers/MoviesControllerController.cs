85525ddcfb8f9b40eaa3b7757deb433d9c3c5c61
shicr <shicr@byu.edu>
2025-02-19 08:48:20 -0700

    :)

---
+++ JoelHiltonFilmCollection/Controllers/MoviesControllerController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoelHiltonFilmCollection.Controllers;
+
+public class MoviesControllerController : Controller
+{
+    public IActionResult Create()
+    {
+        throw new NotImplementedException();
+    }
+}
